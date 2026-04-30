f7fc5e50b3ecb002a66c4f6b40b5565624d1047a
Tony Villegas <luis.villegas@ipejal.gob.mx>
2026-04-30 12:34:07 -0600

    corrección en las versiones

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/menus_comidas/', // Configured for GitHub pages if repo name is menus_comidas
+  base: '/menu_inteligente/', // Configured for GitHub pages repository name 'menu_inteligente'
 })
